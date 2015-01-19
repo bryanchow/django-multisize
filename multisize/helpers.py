efe639de9c5deb552981235579e0584aa59f6655
Bryan Chow <bryan@fullfactor.com>
2015-01-19 11:39:08 -0800

    Add remove_resized_images helper function

--- multisize/helpers.py
+++ multisize/helpers.py
@@ -39,3 +39,17 @@ def get_resized_image(source_instance,
         )
     except ResizedImage.DoesNotExist:
         pass
+
+
+def remove_resized_images(source_instance,
+                          source_field_name):
+    """
+    Remove all ResizedImage instances for the specified object instance and
+    field.
+    """
+
+    return ResizedImage.objects.filter(
+        source_type = get_type_for_instance(source_instance),
+        source_field_name = source_field_name,
+        source_id = source_instance.id,
+    ).delete()
